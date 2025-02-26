1d45630280080779b5e162b8ae28ade353c3d7d3
UpekshaHerath <upekshadilshan000@gmail.com>
2025-02-26 14:54:41 +0530

    fix: removed polyline

--- front_end/src/components/Map.jsx
+++ front_end/src/components/Map.jsx
@@ -3,7 +3,6 @@ import {
   GoogleMap,
   useLoadScript,
   Marker,
-  Polyline,
 } from "@react-google-maps/api";
 
 const mapContainerStyle = {
@@ -14,19 +13,6 @@ const mapContainerStyle = {
 const center = {
     lat: 37.7749, // Latitude for San Francisco
     lng: -122.4194, // Longitude for San Francisco
-};
-
-const pathCoordinates = [
-  { lat: 37.7749, lng: -122.4194 }, // San Francisco
-  { lat: 34.0522, lng: -118.2437 }, // Los Angeles
-  { lat: 36.1699, lng: -115.1398 }, // Las Vegas
-];
-
-const polylineOptions = {
-  strokeColor: "#FF0000", // Red color
-  strokeOpacity: 1.0,
-  strokeWeight: 2,
-  geodesic: true,
 };
 
 const Map = () => {
@@ -45,7 +31,6 @@ const Map = () => {
         center={center}
       >
         <Marker position={center} />
-        <Polyline path={pathCoordinates} options={polylineOptions} />
       </GoogleMap>
     </div>
   );
